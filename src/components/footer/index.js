1fd27b3fe0a4e7177a8d236091a8fc7c3047fe31
Gabriel Lima <gabriellima.silva96@gmail.com>
2019-06-16 15:27:34 -0300

    Adicionando page home

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>Desafio FullStack da Senior Sistemas</p>
+    <p>
+      ©2019 Created by
+      {' '}
+      <a href="https://gabriellima.site">Gabriel Lima</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/pages/home/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../components/footer';
+
+import './style.css';
+
+const Home = () => (
+  <>
+    <header className="header">
+      <h1>ACME</h1>
+      <p>Plataforma de aprovação de fluxo de compras</p>
+    </header>
+    <section className="container">
+      <header>
+        <h2>Você deseja entrar como?</h2>
+      </header>
+      <main className="center">
+        <ul>
+          <li>
+            <Link to="/solicitante" className="button button-outline block">
+              <span>Solicitante</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/administrativo" className="button button-outline my-5 block">
+              <span>Administrativo</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/almoxarife" className="button button-outline block">
+              <span>Almoxarife</span>
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </section>
+    <Footer />
+  </>
+);
+
+export default Home;
