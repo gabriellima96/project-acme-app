7224dedf6cc2d733bdf71fd3e4e4bafa4ee0807c
Gabriel Lima <gabriellima.silva96@gmail.com>
2019-06-17 09:14:34 -0300

    Criando a rota e página para almoxarife

---
+++ src/pages/almoxarife/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Almoxarife = () => <h1>Em desenvolvimento</h1>;
+
+export default Almoxarife;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
 import OrderRequest from './pages/orderRequest';
 import OrderFilter from './pages/orderFilter';
+import Almoxarife from './pages/almoxarife';
 
 const Routes = () => (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
     <Route path="/solicitante" component={OrderRequest} />
     <Route path="/administrativo" component={OrderFilter} />
+    <Route path="/almoxarife" component={Almoxarife} />
   </BrowserRouter>
 );
 
